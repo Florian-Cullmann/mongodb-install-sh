23af755c689dca1c42bd9d01bc81685ccb58386e
Florian Cullmann <florian@fcullmann.com>
2021-08-06 22:43:36 +0200

    init script commit

---
+++ install_mongodb.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | apt-key add -
+echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/5.0 main" | tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+apt update
+apt-get install -y mongodb-org
+systemctl start mongod
+systemctl status mongod
