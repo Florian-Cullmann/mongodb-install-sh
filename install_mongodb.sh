e55ad2b95f13b4173013ce517d17aa290e9bd621
Florian Cullmann <66006032+Florian-Cullmann@users.noreply.github.com>
2023-04-07 20:25:59 +0200

    Update install_mongodb.sh
    fix for plesk with php 8.2

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | apt-key add -
-echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/5.0 main" | tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+echo "deb [arch=amd64] http://repo.mongodb.org/apt/debian buster/mongodb-org/5.0 main" | tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 apt update
 apt-get install -y mongodb-org
 systemctl start mongod
 systemctl status mongod
-pecl install mongodb
+/opt/plesk/php/8.2/bin/pecl install mongodb
 service apache2 restart
